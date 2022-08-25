0504e9880e002f348fe410ffca1cd3d9792ee96c
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-08-26 01:49:41 +0200

    changess

--- src/App.js
+++ src/App.js
@@ -13,48 +13,4 @@ function App() {
 }
 
 
-function DreamsPlayer() {
-
-  const {currentDream, increaseDreamIndex }  = useDreams();
-
-  console.log("useDreams", currentDream);
-
-  return <><DreamPlayer dream={currentDream} donePlaying={increaseDreamIndex} /></>
-}
-
-function DreamPlayer({ donePlaying, dream }) {
-
-  const dreamResultsID = useDreamResults(dream);
-  const data = useIPFS(dreamResultsID);
-
-  const dreamVideoURL = data?.output && data?.output["out_0.mp4"];
-  // useInterval(donePlaying, 10000);
-  console.log("dreamVideoURL", dreamVideoURL, data);
-  return <>
-  {JSON.stringify(data)}
-    <video controls onEnded={donePlaying} autoPlay playsInline muted >
-      <source src={dreamVideoURL} type="video/mp4" />
-    </video>
-  </>
-}
-
-
-
 export default App;
-
-
-
-// gets the output pollen content id for a given pollen input id
-function useDreamResults(dream) {
-  
-  const [dreamResultsID, setDreamResultsID] = useState(null);
-
-  useEffect(() => {
-    if (dream?.dreamID) {
-      (async () => setDreamResultsID(await getDreamResults(dream.dreamID)))();
-    }
-  }
-  , [dream?.dreamID]);
-
-  return dreamResultsID;
-}
--- src/components/DreamPlayer.js
+++ src/components/DreamPlayer.js
@@ -15,7 +15,7 @@ export default function DreamPlayer() {
             playsInline 
             muted 
             src={dreamVideoURL}/>
-            
+
         }
     </Container>
 }
