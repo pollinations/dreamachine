c170628c9503e696fb95ad07657e35947fc6b36f
voodoohop <t.haferlach@gmail.com>
2022-08-25 21:47:32 +0200

    factor out form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import useAWSNode from '@pollinations/ipfs/reactHooks/useAWSNode';
 import useIPFS from '@pollinations/ipfs/reactHooks/useIPFS';
-import { append, last, update } from "ramda";
 import { useEffect, useState } from "react";
 import useInterval from 'use-interval';
 import './App.css';
-import { getDreamResults, getDreams, setDreams } from "./dreamStore";
+import { DreamForm } from './DreamForm';
+import { getDreamResults, getDreams } from "./dreamStore";
 
 function App() {
 
@@ -44,52 +43,6 @@ function DreamPlayer({ donePlaying, dream }) {
 }
 
 
-
-function DreamForm() {
-
-  const [dreamPrompt, setDreamPrompt] = useState("");
-  const { submitToAWS } = useAWSNode({});
-
-  const dispatchDream = async (event) => {
-    event.preventDefault();
-
-    console.log("dispatching dream", dreamPrompt);
-
-    const dreamsUntilNow = await getDreams();
-    const lastDreamIndex = dreamsUntilNow.length - 1;
-    const lastDream = dreamsUntilNow[lastDreamIndex];
-    
-    const lastDreamPrompt = last(lastDream.dream.split("\n"));
-
-    const dreamWithLast = !lastDream ? dreamPrompt : `${lastDreamPrompt}\n${dreamPrompt}`;
-
-    const dreamsWithNewOne = append({ dream: dreamWithLast }, dreamsUntilNow);
-
-    const currentDreamIndex = dreamsWithNewOne.length - 1;
-    
-    console.log("dreamsWithNewOne", dreamsWithNewOne, "updating db");
-    await setDreams(dreamsWithNewOne);
-    
-    const { nodeID } = await submitToAWS({ prompts: dreamWithLast }, "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private");
-    
-    const newDreams = update(currentDreamIndex, 
-      { 
-        dream: dreamWithLast, 
-        dreamID: nodeID,
-      }, 
-      dreamsWithNewOne);
-
-    console.log("dream submitted, nodeID:", nodeID, "new dreams:", newDreams);
-    await setDreams(newDreams);
-  }
-  
-  return (
-    <form onSubmit={dispatchDream}>
-      <input type="text" name="dream" onChange={event => setDreamPrompt(event.target.value)} />
-      <input type="submit" value="Submit" />
-    </form>
-  );
-}
 
 export default App;
 
---
+++ src/DreamForm.js
@@ -0,0 +1,50 @@
+import useAWSNode from '@pollinations/ipfs/reactHooks/useAWSNode';
+import { append, last, update } from "ramda";
+import { useState } from "react";
+import { getDreams, setDreams } from "./dreamStore";
+
+export function DreamForm() {
+
+  const [dreamPrompt, setDreamPrompt] = useState("");
+  const { submitToAWS } = useAWSNode({});
+
+  const dispatchDream = async (event) => {
+    event.preventDefault();
+
+    console.log("dispatching dream", dreamPrompt);
+
+    const dreamsUntilNow = await getDreams();
+    const lastDreamIndex = dreamsUntilNow.length - 1;
+    const lastDream = dreamsUntilNow[lastDreamIndex];
+
+    const lastDreamPrompt = last(lastDream.dream.split("\n"));
+
+    const dreamWithLast = !lastDream ? dreamPrompt : `${lastDreamPrompt}\n${dreamPrompt}`;
+
+    const dreamsWithNewOne = append({ dream: dreamWithLast }, dreamsUntilNow);
+
+    const currentDreamIndex = dreamsWithNewOne.length - 1;
+
+    console.log("dreamsWithNewOne", dreamsWithNewOne, "updating db");
+    await setDreams(dreamsWithNewOne);
+
+    const { nodeID } = await submitToAWS({ prompts: dreamWithLast }, "614871946825.dkr.ecr.us-east-1.amazonaws.com/pollinations/stable-diffusion-private");
+
+    const newDreams = update(currentDreamIndex,
+      {
+        dream: dreamWithLast,
+        dreamID: nodeID,
+      },
+      dreamsWithNewOne);
+
+    console.log("dream submitted, nodeID:", nodeID, "new dreams:", newDreams);
+    await setDreams(newDreams);
+  };
+
+  return (
+    <form onSubmit={dispatchDream}>
+      <input type="text" name="dream" onChange={event => setDreamPrompt(event.target.value)} />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
