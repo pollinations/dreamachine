8656832f6d01ffa969599894bb72a090b1dfd118
voodoohop <t.haferlach@gmail.com>
2023-09-01 18:28:48 +0200

    Fix styles

--- src/dreamStore.js
+++ src/dreamStore.js
@@ -127,13 +127,13 @@ const filterDreams = ({loading, videoURL}) => loading === false && videoURL
 
 
 
-const surrealistPromptPimper1 = prompt => `Dream of ${prompt}. ${prompt}. Surrealism. Klarwein, Dali, Magritte`;
-const surrealistPromptPimper2 = prompt => `Dream of ${prompt}. ${prompt}. surrealistic. illustration. painting. Hand drawn. Black and white.`;
+const surrealistPromptPimper1 = prompt => `${prompt}. Surrealism. Klarwein, Dali, Magritte`;
+const surrealistPromptPimper2 = prompt => `${prompt}. surrealistic. illustration. painting. Hand drawn. Black and white.`;
 const risographPromptPimper3 = prompt => `${prompt}. Risograph. Minimalism.`;
 const retroFuturisticPromptPimper4 = prompt => `Retro-futurist ${prompt}. Poster. vintage sci-fi, 50s and 60s style, atomic age, vibrant,`;
 const vintagePhotoPimper = prompt => `analog film photo ${prompt} . faded film, desaturated, 35mm photo, grainy, vignette, vintage, Kodachrome, Lomography, stained, highly detailed, found footage`;
 const solarPunkPromptPimper = prompt => `A solarpunk ${prompt}, high resolution, neon lights, light and shadow`;
-const graffitiPromptPimper = prompt => `Dream of ${prompt}. graffiti art, inspired by Brad Kunkle, tutu, russ mills, andrey gordeev`
+const graffitiPromptPimper = prompt => `${prompt}. graffiti art, inspired by, andrey gordeev`
 const paperQuilling = prompt => `paper quilling art of ${prompt} . intricate, delicate, curling, rolling, shaping, coiling, loops, 3D, dimensional, ornamental`;
 const paperCut = prompt => `papercut collage of ${prompt} . mixed media, textured paper, overlapping, asymmetrical, abstract, vibrant, dimensional`
 // const pimpDreamPrompts = (prompts) => prompts.split("\n").map(risographPromptPimper3).join("\n");
