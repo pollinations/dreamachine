41dbfcd4388a3b1b41d15c29f8bc7a90bffd0171
voodoohop <t.haferlach@gmail.com>
2023-09-01 17:02:03 +0200

    fix style

--- src/dreamStore.js
+++ src/dreamStore.js
@@ -133,7 +133,7 @@ const risographPromptPimper3 = prompt => `${prompt}. Risograph. Minimalism.`;
 const retroFuturisticPromptPimper4 = prompt => `Retro-futurist ${prompt}. Poster. vintage sci-fi, 50s and 60s style, atomic age, vibrant,`;
 const vintagePhotoPimper = prompt => `analog film photo ${prompt} . faded film, desaturated, 35mm photo, grainy, vignette, vintage, Kodachrome, Lomography, stained, highly detailed, found footage`;
 const solarPunkPromptPimper = prompt => `A solarpunk ${prompt}, high resolution, neon lights, light and shadow`;
-const graffitiPromptPimper = prompt => `Dream of ${prompt}. graffiti art, inspired by Brad Kunkle, tutu, russ mills, hip skirt wings, andrey gordeev`
+const graffitiPromptPimper = prompt => `Dream of ${prompt}. graffiti art, inspired by Brad Kunkle, tutu, russ mills, andrey gordeev`
 const paperQuilling = prompt => `paper quilling art of ${prompt} . intricate, delicate, curling, rolling, shaping, coiling, loops, 3D, dimensional, ornamental`;
 const paperCut = prompt => `papercut collage of ${prompt} . mixed media, textured paper, overlapping, asymmetrical, abstract, vibrant`
 // const pimpDreamPrompts = (prompts) => prompts.split("\n").map(risographPromptPimper3).join("\n");
